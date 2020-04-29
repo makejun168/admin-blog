1ea79d43a8d4430b0b3d68d900dd36ffa41a69b5
makejun168 <873800030@qq.com>
2020-04-29 09:41:01 +0800

    update

--- src/pages/Table/BasicTable.js
+++ src/pages/Table/BasicTable.js
@@ -1,6 +1,7 @@
 import React from "react";
-import { Table, Tag, Card, Row, Col } from "antd";
+import { Table, Tag, Card, Row, Col, Button, Modal, message } from "antd";
 import { getTableList } from "../../api";
+import until from "../../utils/until";
 
 const { Column, ColumnGroup } = Table;
 
@@ -35,97 +36,152 @@ const columns = [
   {
     title: "Id",
     dataIndex: "id",
-    key: '1'
+    key: "1",
   },
   {
     title: "State",
     dataIndex: "state",
-    key: '2',
+    key: "2",
     render(state) {
       // 定义字典
       let config = {
-        '1': '<。)#)))≦',
-        '2':'m( =∩王∩= )m',
-        '3': 'o(=•ェ•=)m',
-        '4': 'U•ェ•*U',
-        '5': '(*^_^*)'
-      }
-      return config[state]
-    }
+        "1": "<。)#)))≦",
+        "2": "m( =∩王∩= )m",
+        "3": "o(=•ェ•=)m",
+        "4": "U•ェ•*U",
+        "5": "(*^_^*)",
+      };
+      return config[state];
+    },
   },
   {
     title: "UserName",
     dataIndex: "userName",
-    key: '3'
+    key: "3",
   },
   {
     title: "Interest",
     dataIndex: "interest",
-    key: '4',
+    key: "4",
     render(interest) {
       // 定义字典
       let config = {
-        '1': '游泳',
-        '2':'跳水',
-        '3': '篮球',
-        '4': '足球',
-        '5': '多人运动',
-        '6': '跳绳',
-        '7': '体操',
-        '8': '铁人三项'
-      }
+        "1": "游泳",
+        "2": "跳水",
+        "3": "篮球",
+        "4": "足球",
+        "5": "多人运动",
+        "6": "跳绳",
+        "7": "体操",
+        "8": "铁人三项",
+      };
       return config[interest];
-    }
+    },
   },
   {
     title: "Birthday",
     dataIndex: "birthday",
-    key: '5'
+    key: "5",
   },
   {
     title: "IsMarried",
     dataIndex: "isMarried",
-    key: '6'
+    key: "6",
   },
   {
     title: "Sex",
     dataIndex: "sex",
-    key: '7',
+    key: "7",
     render(sex) {
-      return sex === 1? '男' : '女'
-    }
+      return sex === 1 ? "男" : "女";
+    },
   },
   {
     title: "Time",
     dataIndex: "time",
-    key: '8'
+    key: "8",
   },
   {
     title: "Address",
     dataIndex: "address",
-    key: '9'
+    key: "9",
   },
+  // {
+  //   title: '操作',
+  //   render: (current) => <Button type="danger" onClick={() => this.handleClick(current)}>删除</Button>
+  // }
 ];
 
 export default class BaiscTable extends React.Component {
-  state = {};
+  state = {
+    selectedRowKeys: [],
+    selectedRow: null,
+  };
+
+  params = {
+    page: 1
+  }
 
   // 动态获取数据
   request() {
-    getTableList({page: 1}).then((res) => {
-      console.log(res);
-      const {result} = res;
+    getTableList({ page: this.params.page }).then((res) => {
+      // console.log(res);
+      const { result } = res;
       this.setState({
-        list: result.list
-      })
+        list: result.list,
+        pagination: until.pagination(res, current => {
+          console.log(current);
+          this.params.page = current;
+          this.request();
+        })
+      });
     });
   }
 
+  handleClickDel = () => {
+    if (this.state.selectedRow.length > 0) {
+      Modal.confirm({
+        title: "tips",
+        content: "Do you want to delete these items?",
+        onOk: () => {
+          message.success("删除成功");
+          this.request(this.params.page);
+          this.setState({
+            selectedRowKeys: [],
+            selectedRow: null
+          });
+          // console.log(this.state.selectedRow);
+        },
+        onCancel() {},
+      });
+    }
+  };
+
   componentDidMount() {
-    this.request();
+    this.request(this.params.page);
   }
 
   render() {
+    const rowSelection = {
+      // 'radio'
+      type: "checkbox",
+      selectedRowKeys: this.state.selectedRowKeys,
+      onChange: (selectedRowKeys, selectedRows) => {
+        this.setState({
+          selectedRowKeys: selectedRowKeys,
+          selectedRow: selectedRows,
+        });
+        console.log(
+          `selectedRowKeys: ${selectedRowKeys}`,
+          "selectedRows: ",
+          selectedRows
+        );
+      },
+      getCheckboxProps: (record) => ({
+        disabled: record.name === "Disabled User", // Column configuration not to be checked
+        name: record.name,
+      }),
+    };
     return (
       <div style={{ width: "100%" }}>
         <Row>
@@ -191,9 +247,22 @@ export default class BaiscTable extends React.Component {
         <Row>
           <Col span={24}>
             <Card>
-              <Table columns={columns} dataSource={this.state.list}></Table>
+              <Table
+                pagination={this.state.pagination}
+                rowKey={(record) => record.id}
+                rowSelection={rowSelection}
+                columns={columns}
+                dataSource={this.state.list}
+              ></Table>
+              <Button type="danger" onClick={this.handleClickDel}>
+                删除多行
+              </Button>
             </Card>
           </Col>
+          <Col span={24}></Col>
+        </Row>
+        <Row>
+          <Col span={24}></Col>
         </Row>
       </div>
     );
--- src/utils/until.js
+++ src/utils/until.js
@@ -1,9 +1,35 @@
 export default {
   formateDate(time) {
     if (!time) {
-      return ''
+      return "";
     }
     let date = new Date(time);
-    return date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate() + " " + date.getHours() + ':' + (date.getMinutes() < 10 ? '0' + date.getMinutes() : date.getMinutes()) + ':' + (date.getSeconds() < 10 ? '0' + date.getSeconds() : date.getSeconds());
-  }
-}
+    return (
+      date.getFullYear() +
+      "-" +
+      (date.getMonth() + 1) +
+      "-" +
+      date.getDate() +
+      " " +
+      date.getHours() +
+      ":" +
+      (date.getMinutes() < 10 ? "0" + date.getMinutes() : date.getMinutes()) +
+      ":" +
+      (date.getSeconds() < 10 ? "0" + date.getSeconds() : date.getSeconds())
+    );
+  },
+
+  pagination(data, callback) {
+    return {
+      onChange: (current) => {
+        callback(current);
+      },
+      current: data.result.page,
+      total: data.result.total_count,
+      pageSize: data.result.page_size,
+      showTotal: () => {
+        return `共${data.result.total_count}条`
+      }
+    };
+  },
+};
