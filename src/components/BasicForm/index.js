eba27b6d0b30584122f657fff2be105ba64f4566
makejun168 <873800030@qq.com>
2020-05-05 18:23:12 +0800

    feat:BasicForm

---
+++ src/components/BasicForm/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import {Input, Select, Form, Button, Checkbox, Radio} from 'antd';
+
+const {Options} = Select;
+
+export default class BasicForm extends React.Component {
+	render() {
+		return <div className="footer">版权所有</div>;
+	}
+}
